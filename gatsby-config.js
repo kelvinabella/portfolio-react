5f2773594849b384c837fe4a7eee94c37def025f
Kelvin Abella <abellavinkel@gmail.com>
2018-02-11 23:10:23 +0400

    add site best practices

--- gatsby-config.js
+++ gatsby-config.js
@@ -190,8 +190,8 @@ module.exports = {
     {
       resolve: 'gatsby-plugin-manifest',
       options: {
-        name: 'GatsbyJS',
-        short_name: 'GatsbyJS',
+        name: 'kelvstudio',
+        short_name: 'kelvstudio',
         start_url: '/',
         background_color: '#f7f0eb',
         theme_color: '#a2466c',
